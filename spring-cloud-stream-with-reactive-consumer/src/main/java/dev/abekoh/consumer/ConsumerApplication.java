693d4bbc6873d8e89b1129a1596dc29fc0f35c32
Kotaro Abe <abekoh.bcky@gmail.com>
2021-10-12 16:01:55 +0900

    add @SpringBootApplication

--- spring-cloud-stream-with-reactive-consumer/src/main/java/dev/abekoh/consumer/ConsumerApplication.java
+++ spring-cloud-stream-with-reactive-consumer/src/main/java/dev/abekoh/consumer/ConsumerApplication.java
@@ -1,7 +1,9 @@
 package dev.abekoh.consumer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
--- spring-cloud-stream-with-reactive-supplier/src/main/java/dev/abekoh/supplier/SupplierApplication.java
+++ spring-cloud-stream-with-reactive-supplier/src/main/java/dev/abekoh/supplier/SupplierApplication.java
@@ -1,7 +1,9 @@
 package dev.abekoh.supplier;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SupplierApplication {
 
 	public static void main(String[] args) {
