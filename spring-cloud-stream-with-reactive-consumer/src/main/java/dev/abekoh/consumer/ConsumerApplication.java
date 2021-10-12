262aaff323182509094cc77dcb3ca57c54caabab
Kotaro Abe <abekoh.bcky@gmail.com>
2021-10-12 15:55:40 +0900

    update structure

---
+++ spring-cloud-stream-with-reactive-consumer/src/main/java/dev/abekoh/consumer/ConsumerApplication.java
@@ -0,0 +1,11 @@
+package dev.abekoh.consumer;
+
+import org.springframework.boot.SpringApplication;
+
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-stream-with-reactive-supplier/src/main/java/dev/abekoh/supplier/SupplierApplication.java
@@ -0,0 +1,11 @@
+package dev.abekoh.supplier;
+
+import org.springframework.boot.SpringApplication;
+
+public class SupplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierApplication.class, args);
+	}
+
+}
