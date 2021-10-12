3cef8784ef1da338bbd0b261c58edaf1742d4ea8
Kotaro Abe <abekoh.bcky@gmail.com>
2021-10-12 15:37:36 +0900

    initialize

---
+++ src/test/java/dev/abekoh/springcloudstreamwithreactive/SpringCloudStreamWithReactiveApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.abekoh.springcloudstreamwithreactive;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamWithReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
