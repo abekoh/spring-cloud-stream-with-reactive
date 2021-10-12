33b1bc4912875caba744026eb552130315b753ce
Kotaro Abe <abekoh.bcky@gmail.com>
2021-10-12 18:18:34 +0900

    add UserReceiver

---
+++ spring-cloud-stream-with-reactive-consumer/src/main/java/dev/abekoh/consumer/UserReceiver.java
@@ -0,0 +1,19 @@
+package dev.abekoh.consumer;
+
+
+import dev.abekoh.domain.models.User;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UserReceiver {
+
+	public Mono<Void> receiveUser(User user) {
+		log.info("receive {}", user);
+		return Mono.empty();
+	}
+
+}
